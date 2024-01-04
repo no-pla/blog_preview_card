e784016a703a53c44b23faa64db3940841f003d1
no-pla <songjihyun323@gmail.com>
2024-01-04 16:45:19 +0900

    Design: 유저 프로필 정보 스타일 add

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Card from "./components/Card";
 import PostDescription from "./components/PostDescription";
+import UserInfo from "./components/UserInfo";
 
 export default function Home() {
   return (
     <main className="bg-yellow h-screen flex justify-center items-center">
       <Card>
         <PostDescription />
+        <UserInfo />
       </Card>
     </main>
   );
