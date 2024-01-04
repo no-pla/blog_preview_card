7df4b784168ddf246a0817b9c1a0081d70a50b3c
no-pla <songjihyun323@gmail.com>
2024-01-04 17:15:28 +0900

    Design: 모바일 반응형 디자인 add

--- app/components/PostDescription.tsx
+++ app/components/PostDescription.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const PostDescription = () => {
   return (
     <div className="flex flex-col gap-3">
-      <div className="px-3 py-1 bg-yellow w-fit font-extrabold rounded">
+      <div className="px-3 py-1 bg-yellow w-fit font-extrabold rounded sm:text-[12px]">
         Learning
       </div>
-      <span className="text-sm">Published 12 Dec 2023</span>
-      <div className="leading-normal text-2xl font-extrabold hover:text-yellow transition ease-in-out duration-300 cursor-pointer">
+      <span className="text-sm sm:text-[12px]">Published 12 Dec 2023</span>
+      <div className="leading-normal text-2xl font-extrabold hover:text-yellow transition ease-in-out duration-300 cursor-pointer sm:text-[20px]">
         HTML & CSS foundations
       </div>
       <p className="text-grey leading-normal sm:text-[14px]">
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Card from "./components/Card";
 import PostDescription from "./components/PostDescription";
+import PostImage from "./components/PostImage";
 import UserInfo from "./components/UserInfo";
 
 export default function Home() {
   return (
     <main className="bg-yellow h-screen flex justify-center items-center">
       <Card>
+        <PostImage />
         <PostDescription />
         <UserInfo />
       </Card>
